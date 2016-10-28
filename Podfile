8d442e412597e1e7f17e4e5e70cd2032241fb2d2
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-10-28 17:04:10 -0500

    Localize-Swift
    Switches between 3 languages.

--- Podfile
+++ Podfile
@@ -1,4 +1,5 @@
 # Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
 # platform :ios, '9.0'
 
 target 'SwiftLocalizationExample' do
@@ -7,6 +8,7 @@ target 'SwiftLocalizationExample' do
 
   # Pods for SwiftLocalizationExample
   pod 'IBLocalizable'
+  pod 'Localize-Swift', '~> 1.6'
 
   target 'SwiftLocalizationExampleTests' do
     inherit! :search_paths
--- SwiftLocalizationExample/ViewController.swift
+++ SwiftLocalizationExample/ViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import Localize_Swift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LabelHelloWorld: UILabel!
+    @IBOutlet weak var resetEnglishButton: UIButton!
+    @IBOutlet weak var portugueseButton: UIButton!
+    @IBOutlet weak var russianButton: UIButton!
+    
+    let availableLanguages = Localize.availableLanguages()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.title = NSLocalizedString("nav-title-hey", comment: "Navigation Title: Hey")
+//        self.navigationItem.title = NSLocalizedString("nav-title-hey", comment: "Navigation Title: Hey")
 
         let count = 100
         let localizedString = String.localizedStringWithFormat(NSLocalizedString("%d file(s) remaining", comment: "Message shown for remaining files"), count)
         print("localizedString: \(localizedString)")
+        
+        self.setText()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(setText), name: NSNotification.Name(LCLLanguageChangeNotification), object: nil)
+    }
+    
+    func setText() {
+        self.navigationItem.title = "nav-title-hey".localized()
+        LabelHelloWorld.text = "Main.View.HelloWorldLabel".localized()
+        resetEnglishButton.setTitle("resetEnglishButton".localized(), for: UIControlState.normal)
+        portugueseButton.setTitle("portugueseButton".localized(), for: UIControlState.normal)
+        russianButton.setTitle("russianButton".localized(), for: UIControlState.normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +49,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func resetEnglishButtonTapped(_ sender: AnyObject) {
+        Localize.setCurrentLanguage("en")
+//        Localize.resetCurrentLanguageToDefault()
+    }
 
+    @IBAction func portugueseButtonTapped(_ sender: AnyObject) {
+        Localize.setCurrentLanguage("pt-PT")
+    }
+    
+    @IBAction func russianButtonTapped(_ sender: AnyObject) {
+        Localize.setCurrentLanguage("ru")
+    }
+   
+    
+    
+    
+    
+//    @IBAction func doChangeLanguage(_ sender: AnyObject) {
+//        actionSheet = UIAlertController(title: nil, message: "Switch Language", preferredStyle: UIAlertControllerStyle.actionSheet)
+//        for language in availableLanguages {
+//            let displayName = Localize.displayNameForLanguage(language)
+//            let languageAction = UIAlertAction(title: displayName, style: .default, handler: {
+//                (alert: UIAlertAction!) -> Void in
+//                Localize.setCurrentLanguage(language)
+//            })
+//            actionSheet.addAction(languageAction)
+//        }
+//        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: {
+//            (alert: UIAlertAction) -> Void in
+//        })
+//        actionSheet.addAction(cancelAction)
+//        self.present(actionSheet, animated: true, completion: nil)
+//    }
+//    
+//    @IBAction func doResetLanguage(_ sender: AnyObject) {
+//        Localize.resetCurrentLanguageToDefault()
+//    }
+    
 }
 
