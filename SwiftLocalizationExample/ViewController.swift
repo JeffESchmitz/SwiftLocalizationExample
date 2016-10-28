7644c0efb5d9432148fbbc2d711865accaa82651
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-10-28 13:37:53 -0500

    Added .stringsdict file and russian language

--- SwiftLocalizationExample/ViewController.swift
+++ SwiftLocalizationExample/ViewController.swift
@@ -14,8 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.title = NSLocalizedString("nav-title-hey", comment: "Navigation Title: Hey")
-//        self.navigationItem.title = "foobar"
-        
+
+        let count = 100
+        let localizedString = String.localizedStringWithFormat(NSLocalizedString("%d file(s) remaining", comment: "Message shown for remaining files"), count)
+        print("localizedString: \(localizedString)")
     }
 
     override func didReceiveMemoryWarning() {
